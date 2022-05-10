3c98cd461e43b9258110875f242b076c989ce203
Mae Pena <maepena@Maes-MacBook-Pro.local>
2022-05-09 22:32:57 -0700

    updated controllers

---
+++ migrations/20220508170116-add-recommendation-column-to-bands.js
@@ -0,0 +1,13 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('bands', 'recommendation', {
+      type: DataTypes.STRING
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('bands', 'recommendation')
+  }
+}
+
+
